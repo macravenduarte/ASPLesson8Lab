29b135a3aa0de47592d1a0963286f5c8711f2f80
macravenduarte <duarte.marco242@gmail.com>
2016-07-28 19:19:56 -0400

    finished lesson

--- ASPLesson8Lab/App_Start/BundleConfig.cs
+++ ASPLesson8Lab/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace ASPLesson8Lab
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
--- ASPLesson8Lab/Controllers/HomeController.cs
+++ ASPLesson8Lab/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace ASPLesson8Lab.Controllers
 {
     public class HomeController : Controller
     {
+        //GET: /Home/Index or /
         public ActionResult Index()
         {
             return View();
         }
 
+        //GET: /Home/About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +22,7 @@ namespace ASPLesson8Lab.Controllers
             return View();
         }
 
+        //GET: /Home/Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
